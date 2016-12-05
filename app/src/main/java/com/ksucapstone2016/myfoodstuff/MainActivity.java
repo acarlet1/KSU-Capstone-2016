7cca8115fa692448c989b91cd742dd679f347e49
akenyon2 <akenyon2@kent.edu>
2016-12-05 15:54:03 -0500

    Removed unnecessary imports. Bug in restarting project. Prices appear but are not in the correct order.

--- app/src/main/java/com/ksucapstone2016/myfoodstuff/MainActivity.java
+++ app/src/main/java/com/ksucapstone2016/myfoodstuff/MainActivity.java
@@ -5,82 +5,39 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
-//import android.net.Uri;
 import android.net.Uri;
 import android.os.Bundle;
-
 import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.KeyEvent;
-import android.view.inputmethod.EditorInfo;
 import android.widget.SearchView;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.app.Activity;
-import android.app.ProgressDialog;
-import android.app.usage.UsageEvents;
-import android.os.Bundle;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.view.View.OnKeyListener;
-
 import com.android.volley.Request.Method;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.StringRequest;
-
 import java.text.NumberFormat;
 import java.util.ArrayList;
-//import java.util.Arrays;
-import java.util.Collections;
 import java.text.DecimalFormat;
-
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
-
 import android.app.AlertDialog;
-import android.widget.EditText;
 import android.content.DialogInterface;
-
 import android.widget.Toast;
 import android.widget.AdapterView;
-import android.widget.TextView;
-/*import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;*/
 import android.view.MotionEvent;
-
 import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
-import android.app.SearchManager;
-import android.widget.SearchView.OnQueryTextListener;
-/*import android.view.ScaleGestureDetector;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-
-import android.support.v4.view.GestureDetectorCompat;
-
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
-*/
 
 public class MainActivity extends AppCompatActivity{
-    //ArrayList<String> shoppingList = null; shopping list ---> masterItems
     ArrayAdapter<String> adapter = null;
     ArrayAdapter<Double> adapter2 = null;
     ArrayAdapter<String> adapter3 = null;
@@ -99,21 +56,14 @@ public class MainActivity extends AppCompatActivity{
     ArrayList<String> a_priceString = new ArrayList<String>();
     ArrayList<Float> WalmartPrices = new ArrayList<Float>();
 
-    //String priceNum = null;
     Double PRICE = 0.00;
     Double TOTAL = 0.00;
 
     Double PRICE2 = 0.00;
     Double TOTAL2 = 0.00;
 
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
 
     private String TAG = MainActivity.class.getSimpleName();
-    private EditText btnStringReq;
-    private TextView msgResponse;
     private ProgressDialog pDialog;
     private String result;
 
@@ -137,29 +87,19 @@ public class MainActivity extends AppCompatActivity{
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
         masterItems = getArrayVal(getApplicationContext());
-
-        //btnStringReq = (EditText) findViewById(R.id.item_query);
-
-        //msgResponse = (TextView) findViewById(R.id.msgResponse);
 
         pDialog = new ProgressDialog(this);
         pDialog.setMessage("Loading...");
         pDialog.setCancelable(false);
 
-
-        //Collections.sort(masterItems);
         adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, masterItems);
         adapter2 = new ArrayAdapter<Double>(this, android.R.layout.simple_list_item_1, w_prices);
         adapter3 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, w_priceString);
         adapter4 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, a_priceString);
 
-
         lv = (ListView) findViewById(R.id.listView);
         lv.setAdapter(adapter);
-        //lv2 = (ListView) findViewById(R.id.wPrice);
-        //lv2.setAdapter(adapter2);
         lv3 = (ListView) findViewById(R.id.wPrice);
         lv3.setAdapter(adapter3);
         lv4 = (ListView) findViewById(R.id.aPrice);
@@ -176,7 +116,6 @@ public class MainActivity extends AppCompatActivity{
             }
         }
 
-
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView parent, View view, final int position, long id) {
                 String selectedItem = ((TextView) view).getText().toString();
@@ -188,78 +127,58 @@ public class MainActivity extends AppCompatActivity{
             }
         });
 
-
-        //End of addition//
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        //client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        //client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
-
     }
 
     private void makeStringReq(final String ans) throws Exception{
+        // Show loading message
         showProgressDialog();
-        EditText mEdit;
-        //msgResponse = (TextView) findViewById(R.id.msgResponse);
-
-        //System.out.println("searching for " + ans);
         StringRequest strReq = new StringRequest(Method.GET,
                 "http://api.walmartlabs.com/v1/search?apiKey=52pcepcteuhhwx7gtg5z7dbe&query="
                         + ans, new Response.Listener<String>() {
 
             @Override
             public void onResponse(String response) {
-                //System.out.println("response = " + response);
-
-                //Log.d(TAG, response.toString());
                 result = response;
-                int a = result.indexOf("salePrice");
-                int b = result.indexOf("upc");
-                String finalResult;
-                finalResult = result.substring(a + 11, b - 2);
+                int startIndex = result.indexOf("salePrice");
+                int endIndex = result.indexOf("upc");
 
-                //System.out.println("final result = " + finalResult);
-                //msgResponse.setText(response);
+                // Stores price from JSON into finalResult
+                String finalResult;
+                finalResult = result.substring(startIndex + 11, endIndex - 2);
                 float finalResultFloat = Float.parseFloat(finalResult);
                 PRICE = Double.parseDouble(finalResult);
                 DecimalFormat f = new DecimalFormat("##.00");
-                System.out.println("Walmart Price = " + f.format(finalResultFloat));
                 WalmartPrices.add(finalResultFloat);
 
                 lv3.setAdapter(adapter3);
 
+                // w_prices tracks double value
+                // w_priceString is whats displayed on screen
                 w_prices.add(PRICE);
                 w_priceString.add(f.format(PRICE));
-                System.out.println("Testing prices string: " + w_priceString);
                 w_priceStringTmp.add(f.format(PRICE));
-                System.out.println("Master walmart prices: " + WalmartPrices);
-                hideProgressDialog();
-
-
 
                 // Amazon Prices
-                String toSearch = ans;
                 MyTask myTask = null;
                 try {
-                    myTask = new MyTask(MainActivity.this, lv4, adapter4, ans);
-                    //msgResponse text = (msgResponse)findViewById(R.id.msgResponse);
-                    //System.out.println("request html: " + msgResponse.getText().toString());
+                    myTask = new MyTask(MainActivity.this, adapter4, ans);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
                 try {
-                    myTask.execute().get();
+                    myTask.execute().get(); // .get() waits for AsyncTask to finish
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } catch (ExecutionException e) {
                     e.printStackTrace();
                 }
-                System.out.println("response in main: " + myTask.server_response);
+                // Store price from Amazon + query into amz
                 String amz = myTask.server_response;
                 adapter4.add(amz);
                 lv4.setAdapter(adapter4);
+
+                // Hide loading string
+                hideProgressDialog();
             }
         }, new Response.ErrorListener() {
 
@@ -281,17 +200,12 @@ public class MainActivity extends AppCompatActivity{
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        //^changing this to put the value into a variable
-
-        //MenuInflater inflater = getMenuInflater();
-        //inflater.inflate(R.menu.menu_main, menu);
 
         //For the search function -Adam
         MenuItem item = menu.findItem(R.id.action_search);
         SearchView searchView = (SearchView)item.getActionView();
 
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-
 
             @Override
             public boolean onQueryTextSubmit(String query) {
@@ -301,11 +215,6 @@ public class MainActivity extends AppCompatActivity{
             @Override
             public boolean onQueryTextChange(String newText) {
                 adapter.getFilter().filter(newText);
-
-                // added by Aaron
-                // find index where newText is found in master items
-                // and display w_prices[index] on the adapter3
-
                 boolean exit = false;
                 System.out.println(w_priceString);
                 System.out.println(masterItems);
@@ -338,8 +247,6 @@ public class MainActivity extends AppCompatActivity{
 
             @Override
             public boolean onClose() {
-                System.out.println("Closed search..");
-
                 adapter3.clear();
                 adapter.clear();
                 for (int i = 0; i < w_priceStringTmp.size(); i++){
@@ -357,7 +264,6 @@ public class MainActivity extends AppCompatActivity{
 
         });
 
-
         return true;
         // end search view additions
     }
@@ -368,8 +274,6 @@ public class MainActivity extends AppCompatActivity{
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-
 
         if (id == R.id.action_add) {
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -384,8 +288,6 @@ public class MainActivity extends AppCompatActivity{
                     // adds item user wishes to add to masterItems
                     //masterItems.add(preferredCase(input.getText().toString()) + " $" + priceNum);
                     masterItems.add(preferredCase(input.getText().toString()));
-                    //a_prices.add(2.5);
-                    //a_priceString.add("2.5");
                     NumberFormat f = new DecimalFormat("#0.00");
                     System.out.println("User wants: " + input.getText().toString());
                     try {
@@ -397,7 +299,6 @@ public class MainActivity extends AppCompatActivity{
                     storeArrayVal(masterItems, getApplicationContext());
 
                     lv.setAdapter(adapter);
-                    //lv2.setAdapter(adapter2);
                     lv3.setAdapter(adapter3);
                     lv4.setAdapter(adapter4);
                 }
@@ -427,7 +328,6 @@ public class MainActivity extends AppCompatActivity{
                     a_priceString.clear();
                     storeArrayVal(masterItems, getApplicationContext());
                     lv.setAdapter(adapter);
-                    //lv2.setAdapter(adapter2);
                     lv3.setAdapter(adapter3);
                     lv4.setAdapter(adapter4);
                 }
@@ -454,7 +354,6 @@ public class MainActivity extends AppCompatActivity{
             a_priceString.add(f.format(TOTAL2));
 
             lv.setAdapter(adapter);
-            // Aaron
             lv3.setAdapter(adapter3);
             lv4.setAdapter(adapter4);
         }
@@ -496,10 +395,8 @@ public class MainActivity extends AppCompatActivity{
                 a_prices.remove(position);
                 w_priceString.remove(position);
                 a_priceString.remove(position);
-                //Collections.sort(masterItems);
                 storeArrayVal(masterItems, getApplicationContext());
                 lv.setAdapter(adapter);
-                //lv2.setAdapter(adapter2);
                 lv3.setAdapter(adapter3);
                 lv4.setAdapter(adapter4);
             }
@@ -513,7 +410,6 @@ public class MainActivity extends AppCompatActivity{
         builder.show();
     }
 
-
     public void getTotal(){
         for(int i = 0; i < w_prices.size(); i++){
             TOTAL += w_prices.get(i);
@@ -522,87 +418,42 @@ public class MainActivity extends AppCompatActivity{
             TOTAL2 += a_prices.get(j);
         }
     }
-//ToDO: Long hold for menu option
-
+    //ToDO: Long hold for menu option
     //ToDo: Internet data interface
-//ToDo: Side slides delete items
+    //ToDo: Side slides delete items
     public boolean onTouchEvent(MotionEvent event) {
-
         int action = MotionEventCompat.getActionMasked(event);
 
         switch (action) {
             case (MotionEvent.ACTION_DOWN):
-
                 return true;
             case (MotionEvent.ACTION_MOVE):
-
                 return true;
             case (MotionEvent.ACTION_UP):
-
                 return true;
             case (MotionEvent.ACTION_CANCEL):
-
                 return true;
             case (MotionEvent.ACTION_OUTSIDE):
-
                 return true;
             default:
                 return super.onTouchEvent(event);
         }
     }
 
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-  /*  public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("Main Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                //.setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]")) //Commented out by Josh
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
-    }*/
     @Override
     public void onStart() {
-        super.onStart();// ATTENTION: This was auto-generated to implement the App Indexing API.
-// See https://g.co/AppIndexing/AndroidStudio for more information.
-        //client.connect();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        //client.connect(); //Commented out by Josh
-        //AppIndex.AppIndexApi.start(client, getIndexApiAction()); //Commented out by Josh
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        //AppIndex.AppIndexApi.start(client, getIndexApiAction());
+        super.onStart();
     }
 
     @Override
     public void onStop() {
-        super.onStop();// ATTENTION: This was auto-generated to implement the App Indexing API.
-// See https://g.co/AppIndexing/AndroidStudio for more information.
-        //AppIndex.AppIndexApi.end(client, getIndexApiAction());
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        //AppIndex.AppIndexApi.end(client, getIndexApiAction());  //Commented out by Josh
-        //client.disconnect(); //Commented out by Josh
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        //client.disconnect();
+        super.onStop();
     }
 
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
     public Action getIndexApiAction() {
         Thing object = new Thing.Builder()
-                .setName("Main Page") // TODO: Define a title for the content shown.
+                .setName("Main Page")
+                // TODO: Define a title for the content shown.
                 // TODO: Make sure this auto-generated URL is correct.
                 .setUrl(Uri.parse("http://[ENTER-YOUR-HTTP-HOST-HERE]/main"))
                 .build();
@@ -612,37 +463,3 @@ public class MainActivity extends AppCompatActivity{
                 .build();
     }
 }
-
-
-//-----------------------------------------------------------------------------------
-
-     /*   btnStringReq.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View view, int i, KeyEvent keyEvent) {
-                if (keyEvent.getKeyCode() == keyEvent.KEYCODE_ENTER) {
-                    makeStringReq();
-                }
-                return true;
-            }
-        });*/
-
-// Adding request to request queue
-// AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
-
-
-// Added by Josh
-        /*
-        mEdit = (EditText) findViewById(R.id.item_query);
-        String tempText = mEdit.getText().toString();
-        StringBuilder finaltxtBuilder = new StringBuilder();
-        finaltxtBuilder.append(tempText);
-        char[] textChars = tempText.toCharArray();
-        String spacetxt = " ";
-        for (int i = 0; i < tempText.length(); i++)
-            if (tempText.charAt(i) == spacetxt.charAt(0)) {
-                textChars[i] = '+';
-                finaltxtBuilder.setCharAt(i, textChars[i]);
-            }
-
-        String finalStr = finaltxtBuilder.toString();
-*/
--- app/src/main/java/com/ksucapstone2016/myfoodstuff/MyTask.java
+++ app/src/main/java/com/ksucapstone2016/myfoodstuff/MyTask.java
@@ -2,18 +2,10 @@ package com.ksucapstone2016.myfoodstuff;
 
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.Adapter;
-import android.widget.Button;
-import android.widget.ListView;
 import android.widget.TextView;
-
-
-import com.android.volley.Response;
-
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -21,64 +13,33 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-
-//import info.androidhive.volleyexamples.utils.AsyncResponse;
-
-
-import static java.lang.System.out;
-
-/**
- * Created by Aaron on 12/3/2016.
- */
 
 public class MyTask extends AsyncTask <Void, Integer, String> {
     String server_response;
     Context context;
-    ListView listView;
     ProgressDialog progressDialog;
     TextView textView;
     Adapter adapter;
     String ans;
 
-    MyTask(Context context, ListView listview, Adapter adapter, String ans) throws Exception {
+    MyTask(Context context, Adapter adapter, String ans) throws Exception {
         this.context = context;
-        this.listView = listView;
         this.adapter = adapter;
-        //this.button = button;
         this.ans = ans;
     }
 
 
     @Override
     protected String doInBackground(Void... params) {
-        // connecting to url
         URL url;
         HttpURLConnection urlConnection = null;
 
         try {
-            url = new URL("https://www.amazon.com/s/ref=nb_sb_noss_1?url=search-alias%3Daps&field-keywords=" + ans);
-            urlConnection = (HttpURLConnection) url.openConnection();
-            int responseCode = urlConnection.getResponseCode();
-            if(responseCode == HttpURLConnection.HTTP_OK){
+            url = new URL("https://www.amazon.com/s/ref=nb_sb_noss_1?url=search-alias%3Daps&field-keywords="
+                          + ans); // ans being what is to be queried
+            urlConnection = (HttpURLConnection) url.openConnection(); // open connection to url
+            int responseCode = urlConnection.getResponseCode();  // response code for testing connection
+            if(responseCode == HttpURLConnection.HTTP_OK){  // if successful connection
                 server_response = readStream(urlConnection.getInputStream()); // store html into server_response
                 Log.v("CatalogClient", server_response);
             }
@@ -133,17 +94,17 @@ public class MyTask extends AsyncTask <Void, Integer, String> {
         }
         String toReturn = response.toString();
 
-        // a and b for substring of html (dollar)
-        int b = toReturn.indexOf("class=\"sx-price-fractional") - 32;
-        int a = b-5;
-        toReturn = toReturn.substring(a, b);
+        // substring to find dollar amount
+        int dollarEndIndex = toReturn.indexOf("class=\"sx-price-fractional") - 32;
+        int dollarStartIndex = dollarEndIndex - 5;
+        toReturn = toReturn.substring(dollarStartIndex, dollarEndIndex);
         int dollar = Integer.parseInt(toReturn.replaceAll("[\\D]", ""));
 
-        // c and d for substring of html )cents)
+        // substring to find cents amount
         toReturn = response.toString();
-        int c = b + 59;
-        int d = c + 4;
-        toReturn = toReturn.substring(c, d);
+        int centsStartIndex = dollarEndIndex + 59;
+        int centsEndIndex = centsStartIndex + 4;
+        toReturn = toReturn.substring(centsStartIndex, centsEndIndex);
         int cents = Integer.parseInt(toReturn.replaceAll("[\\D]", ""));
 
         // combine into one string
